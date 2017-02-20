410971a138d1ebc4a822ce78d1e2a7e57e8d903c
Mattias Andersson <mattias.andersson@umu.se>
2017-02-20 10:03:20 +0100

    war builds

--- simone.core/src/main/java/se/uhr/nya/integration/sim/server/boundary/SimulatorApplication.java
+++
@@ -1,9 +0,0 @@
-package se.uhr.nya.integration.sim.server.boundary;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class SimulatorApplication extends Application {
-
-}
