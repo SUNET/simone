041cd6c3359baabd516dd9b97597d9e899904369
Mattias Andersson <mattias.andersson@umu.se>
2017-02-21 16:54:13 +0100

    added docker

--- simone.extension.api/src/main/java/se/uhr/simone/extension/api/Constants.java
+++ simone.extension.api/src/main/java/se/uhr/simone/extension/api/Constants.java
@@ -7,7 +7,7 @@ import javax.ws.rs.core.UriBuilder;
 public class Constants {
 
 	public static final URI BASE_URI =
-			UriBuilder.fromUri(System.getProperty("se.uhr.nya.integration.sim.base.uri", "http://localhost:61080")).build();
+			UriBuilder.fromUri(System.getProperty("se.uhr.nya.integration.sim.base.uri", "http://localhost:8080")).build();
 
 	public static final URI REST_URI = UriBuilder.fromUri(BASE_URI).segment("sim", "api").build();
 
