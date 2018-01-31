492eb4bf0e767a5678fe73a44998a7f5b794fd9b
Mattias Andersson <mattias.andersson@umu.se>
2018-01-31 10:45:15 +0100

    fixed exception during empty database

---
+++ simone/src/test/java/se/uhr/simone/core/entity/SqlScriptReaderTest.java
@@ -0,0 +1,27 @@
+package se.uhr.simone.core.entity;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class SqlScriptReaderTest {
+
+	@Test
+	public void testGetStatements() {
+
+		SqlScriptReader cut = new SqlScriptReader(this.getClass().getResourceAsStream("/test.sql"));
+
+		List<String> stmts = cut.getStatements();
+
+		assertThat(stmts, hasSize(8));
+
+		for (String stmt : stmts) {
+			assertThat(stmt, not(containsString(";")));
+		}
+	}
+}
