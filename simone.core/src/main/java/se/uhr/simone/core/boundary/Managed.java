5b0b907acb9baa8e9a2afe6f34f14f78fc5b4172
Mattias Andersson <mattias.andersson@umu.se>
2023-10-31 10:58:34 +0100

    updated doc

--- simone.core/src/main/java/se/uhr/simone/core/boundary/Managed.java
+++ simone.core/src/main/java/se/uhr/simone/core/boundary/Managed.java
@@ -6,9 +6,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import jakarta.ws.rs.NameBinding;
 
+/**
+ * Indicates that the resource (API) is managed by SimOne, i.e. possible to manipulate with the admin API.
+ */
+
 @Target({ ElementType.TYPE })
 @NameBinding
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Managed {
+
+	/**
+	 * The name of the SimOne instance.
+	 *
+	 * @return The SimOne instance name.
+	 */
+
 	String value() default "";
 }
